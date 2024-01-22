03ee3309a496b2c1c7c448d65f3a4107bb92119c
Tyron Aricum <tyronaricum1@gmail.com>
2024-01-22 19:24:25 +0200

    create: book and review factory with fake data for seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // create 33 - books and create random good reviews
+        Book::factory(33)->create()->each(function (Book $book) {
+            $numReviews = random_int(5, 30);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            Review::factory()->count($numReviews)
+            ->good()
+            ->for($book)
+            ->create();
+        });
+        
+        // create 33 - books and create random average reviews
+        Book::factory(33)->create()->each(function (Book $book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+            ->average()
+            ->for($book)
+            ->create();
+        });
+        // create 33 - books and create random bad reviews
+        Book::factory(33)->create()->each(function (Book $book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)
+            ->bad()
+            ->for($book)
+            ->create();
+        });
+
     }
 }
