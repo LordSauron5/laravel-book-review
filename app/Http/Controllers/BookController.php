1a95ce8fd55716e5e4d7482d2c2272fffb0c05a6
Tyron Aricum <tyronaricum1@gmail.com>
2024-01-28 19:35:11 +0200

    update: add caching to models

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -25,7 +26,9 @@ class BookController extends Controller
             default => $books->latest()
         };
 
-        $books = $books->get();
+        // cache - the results for an hour
+        $cacheKey = 'books: ' . $filter .':' . $title;
+        $books = Cache::remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
@@ -51,14 +54,13 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view(
-            'books.show', 
-            [
-                'book' => $book->load([
-                    'reviews' => fn ($query) => $query->latest()
-                ])
-            ]
-        );
+        $cacheKey = 'book:' . $book->id;
+
+        $book = Cache::remember($cacheKey, 3600, fn() => $book->load([
+            'reviews' => fn ($query) => $query->latest()
+        ]));
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,12 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Cache;
 
 class Review extends Model
 {
     use HasFactory;
 
     protected $fillable = ['review', 'rating'];
+
+    protected static function booted()
+    {
+        static::updated(fn (Review $review) => Cache::forget('book:'. $review->book_id));
+        static::deleted(fn (Review $review) => Cache::forget('book:'. $review->book_id));
+    }
+
 
     public function book()
     {
